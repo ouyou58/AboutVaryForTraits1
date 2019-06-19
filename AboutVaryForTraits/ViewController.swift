d7e573923b1a5a5627473c47f66acc0c2e56e7f9
ouyou58 <ouyou58@gmail.com>
2019-06-19 11:00:19 +0900

    Initial Commit

---
+++ AboutVaryForTraits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutVaryForTraits
+//
+//  Created by ouyou on 2019/06/19.
+//  Copyright © 2019 ouyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
